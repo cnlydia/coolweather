926c31c80723d7615da188bee67c9a8d3b38ac7c
lydia <cn_lydia@163.com>
2015-12-23 07:49:36 +0800

    新增数据库帮助类，以及各表对应的实体类。

---
+++ src/com/li/coolweather/model/Province.java
@@ -0,0 +1,31 @@
+package com.li.coolweather.model;
+
+public class Province {
+	private int id;
+	private String provinceCode;
+	private String provinceName;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getProvinceCode() {
+		return provinceCode;
+	}
+
+	public void setProvinceCode(String provinceCode) {
+		this.provinceCode = provinceCode;
+	}
+
+	public String getProvinceName() {
+		return provinceName;
+	}
+
+	public void setProvinceName(String provinceName) {
+		this.provinceName = provinceName;
+	}
+}
